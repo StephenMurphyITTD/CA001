7e4958be6c431c9d51d794ed9dde326637514401
Stephen Murphy ITTD <Stephen_Murphy@ittd.ie>
2016-03-03 09:30:09 +0000

    ASP .Net Web Application

---
+++ CA001/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CA001.MvcApplication" Language="C#" %>
---
+++ CA001/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CA001.Startup))]
+namespace CA001
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
